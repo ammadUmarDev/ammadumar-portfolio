93027e8182b8bf559994e9445cb4c50392a80fde
Ammad Umar <ammad.umar@outlook.com>
2021-01-11 19:37:13 +0500

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { React, useEffect } from "react";
-import ReactGA from "react-ga";
+import { React } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
